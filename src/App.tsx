ecffdb0ef569517a0ac3bffa8ab9e14a8795fb92
[dyad] <suporteotaviobastos@gmail.com>
2025-12-01 16:22:28 -0300

    [dyad] Adicionando nova rota com prefixo numérico - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import UserPage from "./pages/UserPage"; // Importar a nova página
+import UserPage from "./pages/UserPage";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/:slug" element={<UserPage />} /> {/* Nova rota para o perfil do usuário */}
+          <Route path="/:slug" element={<UserPage />} />
+          <Route path="/2453985/:slug" element={<UserPage />} /> {/* Nova rota para o perfil do usuário com prefixo */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
