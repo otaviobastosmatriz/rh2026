112825ee8caee18c9fd1a2df801d0ede3445aa6c
[dyad] <suporteotaviobastos@gmail.com>
2025-12-02 20:26:11 -0300

    [dyad] Atualizando a imagem do perfil do usuário - wrote 2 file(s)

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -160,7 +160,7 @@ const UserPage = () => {
             <div className="text-center p-5 flex-shrink-0">
               <img
                 className="img-fluid rounded-full border-4 border-heliopurple w-40 h-40 object-cover mx-auto"
-                src="/images/heliocouto.webp"
+                src="/images/heliocouto.jpg" // Caminho da imagem atualizado
                 alt="Professor Hélio Couto"
               />
             </div>
